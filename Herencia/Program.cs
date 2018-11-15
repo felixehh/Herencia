dc20a46bc76ee9e5fa8b46cc672a06e2a1768e60
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-11-15 08:49:13 -0700

    Agregar archivos de proyecto.

---
+++ Herencia/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AnimalAcuatico delfin = new AnimalAcuatico();
+            Animal elefante = new Animal();
+
+            delfin.Nombre = "Delfin";
+            elefante.Nombre = "Elefante";
+
+            delfin.Comer();
+
+            Console.ReadLine();
+        }
+    }
+}
